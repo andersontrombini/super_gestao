9fd82857c01b8b61e2db7055b9546556bf24e13c
anderson <anderson@viafast.com.br>
2022-08-18 13:48:41 -0300

    criado tabela e update para fornecedores

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,8 @@ Route::middleware('autenticacao:padrao,visitante')
         Route::get('/fornecedor', [FornecedorController::class, 'index'])->name('app.fornecedor');
         Route::post('/fornecedor/listar', [FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
         Route::get('/fornecedor/adicionar', [FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
+        Route::post('/fornecedor/adicionar', [FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
+        Route::get('/fornecedor/editar/{id}/{msg?}', [FornecedorController::class, 'editar'])->name('app.fornecedor.editar');
 
         Route::get('/produto', [ProdutoController::class, 'index'])->name('app.produto');
 });
